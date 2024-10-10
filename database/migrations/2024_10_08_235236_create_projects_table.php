a747d2e44a9ae71f8817b81a72b6a05a6b60d91f
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-10-10 10:03:24 -0300

    final php rocketseat project

--- database/migrations/2024_10_08_235236_create_projects_table.php
+++ database/migrations/2024_10_08_235236_create_projects_table.php
@@ -4,19 +4,17 @@ use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-// use Illuminate\Support\Facades\User;
 
 
 return new class extends Migration
 {
-   
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->dateTime('ends-at');
+            $table->dateTime('ends_at');
             $table->string('status')->default('open');
             $table->json('tech_stack');
             $table->foreignIdFor(User::class, 'created_by')->constrained('users');
